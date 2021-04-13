e5f32249f6b12ae0b9f325397f470cb9402e0c68
JeanMarc <jm.prudhomme@icloud.com>
2021-04-13 11:16:37 -0400

    Alter tables

--- alter.sql
+++ alter.sql
@@ -17,7 +17,7 @@ WHERE personne_id = 30;
 
 UPDATE equipe_foot
 SET placement  = 1
-WHERE nation = 'Italie' AND (edition_coupe = 5 OR edition_coupe = 3);
+WHERE (nation = 'Italie' AND edition_coupe = 5) OR (nation = 'Italie' AND edition_coupe = 3);
 
 UPDATE equipe_foot
 SET placement  = 2
@@ -26,5 +26,14 @@ WHERE nation = 'Belgique' AND edition_coupe = 5;
 UPDATE equipe_foot
 SET placement  = 2
 WHERE nation = 'Russie' AND edition_coupe = 3;
+
+UPDATE equipe_foot 
+set placement = 7
+where nation = 'Russie' AND placement = 1;
+
+
+UPDATE equipe_foot 
+set placement = 7
+where nation = 'Japon' AND placement = 1;
 
 commit;
